4b9be181cc020111faf4a1fa46fb9423266528bc
Edwin Eefting <edwin@datux.nl>
2022-10-23 12:33:23 +0200

    ldr

--- src/sensor-test.cpp
+++ src/sensor-test.cpp
@@ -23,7 +23,7 @@ loop()
     delay(250);
 
   sprintf(buffer,
-          "Kleuren    : (%3d, %3d, %3d)  (%3d, %3d, %3d) ",
+          "Kleuren     : (%3d, %3d, %3d)  (%3d, %3d, %3d) ",
           AIStarter_SmartBotGetColorSenor(COLORSENOR1, RCOLOR),
           AIStarter_SmartBotGetColorSenor(COLORSENOR1, GCOLOR),
           AIStarter_SmartBotGetColorSenor(COLORSENOR1, BCOLOR),
@@ -34,7 +34,7 @@ loop()
   Serial.println(buffer);
 
   sprintf(buffer,
-          "Lijn volger: %d%d%d%d%d",
+          "Lijn volger : %d%d%d%d%d",
           AIStarter_SmartBotGetIRModuleValue(IR1),
           AIStarter_SmartBotGetIRModuleValue(IR2),
           AIStarter_SmartBotGetIRModuleValue(IR3),
@@ -43,6 +43,13 @@ loop()
           AIStarter_SmartBotGetIRModuleValue(IR6)  );
   Serial.println(buffer);
 
+  sprintf(buffer,
+          "Licht sensor: %d",
+          AIStarter_SmartBotGetLightAnalog());
+  Serial.println(buffer);
+
+  sprintf(buffer, "Licht sensor: %d", AIStarter_SmartBotGetBarrier);
+  Serial.println(buffer);
 
   AIStarter_ColorFlash();
 }
